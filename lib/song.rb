6c2f574c2daa32e6bb7f6a8b6d6350d7e86106aa
Laura Darlak <laura@polyporedesign.com>
2018-08-30 01:04:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,12 @@ class Song
   attr_accessor :name, :genre, :artist
   
   @@song = 0
+  @@all = []
   
   def initialize(name)
     @name = name
     @@song += 1
+    @@all << name
   end
   
   def self.song_count
